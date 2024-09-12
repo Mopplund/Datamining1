36c9395d2a9b6f3dce96a7766f2e574ce07c219b
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2024-09-12 15:50:04 +0200

    Almost done

--- Assignment1/miner.py
+++ Assignment1/miner.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 from scipy import stats
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.decomposition import PCA
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 
 
@@ -66,19 +68,94 @@ print('===========')
 print(f"sl mean: {data['sl'].mean()}")
 print(f"sl std: {np.std(data['sl'])}")
 
+print('===========')
+
 # 4.1
-#minmax_scaled = MinMaxScaler().fit_transform(data)
+scaler = MinMaxScaler((0, 1))
+
+# Apply MinMaxScaler to each column and store the result in a new column
+minmax_scaled = scaler.fit_transform(data[['sl']])
+
+print(f"minmax_scaled: sl mean: {minmax_scaled.mean()}")
+print(f"minmax_scaled:sl std: {np.std(minmax_scaled)}")
+
+print('===========')
+
 
 # 4.2
-#sd_scaled = StandardScaler().fit_transform(data)
+sd_scaled = StandardScaler().fit_transform(data[['sl']])
+print(f"sd_scaled: sl mean: {sd_scaled.mean()}")
+print(f"sd_scaled: sl std: {np.std(sd_scaled)}")
+
+print('===========')
+
 
 # 4.3
-#pca = PCA()
-#principal_components = pca.fit_transform(data)
+# Apply PCA with N principal components
+pca = PCA(n_components=4)
+print(pca.explained_variance_ratio_)
+
+n=-1
+for i in range(0,5):
+    if pca.explained_variance_ratio_[0:i].cumsum() >= 0.95:
+        n = i
+        break
+print(f"95% requires {n} components")
+
+principal_components = pca.fit_transform(data[['sl', 'sw', 'pl', 'pw']])
+
+# Check how many components were selected
+print(f"Number of components selected: {pca.n_components_}")
+
+print('===========')
 
 # 4.4
+# Create a DataFrame of the PCA components
+# print(pca.components_.shape)
 
+# pca_df = pd.DataFrame(
+#     pca.components_,
+#     columns=["Sepal L", "Sepal W", "Petal L", "Petal W"],
+#     index=[f'PC {i+1}' for i in range(pca.components_.shape[0])]  # Adjust row labels dynamically
+# )
 
+# # Check the absolute contribution of each attribute to the principal components
+# mean_contribution = pca_df.abs().mean(axis=0)
+# print(mean_contribution)
+
+# 4.5
+#minmax_scaled = MinMaxScaler((0, 100)).fit_transform(data)
+
+def print_repeated_identifiers(m, data):
+    print(f"{m}: Repeats: {data['id'].duplicated().any()}")
+
+# Sampling
+
+# 5.1
+sample = data.sample(n = 150)
+print_repeated_identifiers("Sampling", sample)
+print(sample.value_counts("species"))
+
+print('===========')
+
+# 5.2
+sample = data.sample(n = 150, replace=True)
+print_repeated_identifiers("Bootsraping", sample)
+print(sample.value_counts("species"))
+
+print('===========')
+
+# 5.3
+sample = data.groupby('species' , group_keys = False ).apply(lambda x : x.sample(frac =0.5))
+print_repeated_identifiers("Stratified", sample)
+print(sample.value_counts('species'))
+
+print('===========')
+
+# 5.4
+sample = data.groupby('species' , group_keys = False).apply(lambda x : x.sample(50))
+print_repeated_identifiers("Stratified ver. 2", sample)
+print(sample.value_counts('species'))
 # Plot the pairplot with the filtered data and "species" as hue
-# sns.pairplot(data, hue="species")
-# plt.show()
+#sns.pairplot(data, hue="species")
+#plt.show()
