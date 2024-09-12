a164666933d046e1c9dce3794898aaf48b4ee5b0
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2024-09-12 10:39:35 +0200

    Task 1 completed

--- Assignment1/miner.py
+++ Assignment1/miner.py
@@ -1,5 +1,30 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
-iris = pd.read_csv('iris_data.csv')
-iris = pd.read_csv('iris_labels.csv')
+data = pd.read_csv('iris_data.csv', sep=';')
+labels = pd.read_csv('iris_labels.csv', sep=';')
+
+print(data.shape)
+print(data.head())
+print('===========')
+
+data = pd.merge(data, labels, on = 'id', how = 'inner')
+
+print(data.head())
+print('===========')
+
+data.drop(['examiner'], axis = 1, inplace = True)
+
+print(data.head())
+print('===========')
+
+data = data.sort_values('species')
+
+print(data.head())
+print('===========')
+
+
+sns.pairplot(data, hue = "species")
+plt.show()
