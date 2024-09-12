83ef9513d8c7288f8eb7ee3fc55273b5d580fd8b
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2024-09-12 12:28:37 +0200

    Completed task 3

--- Assignment1/miner.py
+++ Assignment1/miner.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
+
+
 
 data = pd.read_csv('iris_data.csv', sep=';')
 labels = pd.read_csv('iris_labels.csv', sep=';')
@@ -32,5 +37,48 @@ print('===========')
 
 print(data.value_counts("species"))
 
-#sns.pairplot(data, hue = "species")
-#plt.show()
+print('===========')
+
+
+data = data[data["sl"] != -9999]
+data = data[data["sw"] != -9999]
+
+print(f"sl mean: {data['sl'].mean()}")
+print(f"sl std: {np.std(data['sl'])}")
+
+print(f"sw mean: {data['sw'].mean()}")
+print(f"sw std: {np.std(data['sw'])}")
+
+# Calculate the Z-scores of 'sl'
+sl_z = stats.zscore(np.array(data['sl']))
+print(np.sort(stats.zscore(np.array(data['sl']))))
+
+# Filter out rows where the absolute value of Z-scores is greater than or equal to 15
+# We'll only keep the original rows where Z-scores satisfy this condition
+data = data[np.abs(sl_z) < 10]
+
+sw_z = stats.zscore(np.array(data['sw']))
+data = data[np.abs(sw_z) < 10]
+
+print('===========')
+
+
+print(f"sl mean: {data['sl'].mean()}")
+print(f"sl std: {np.std(data['sl'])}")
+
+# 4.1
+#minmax_scaled = MinMaxScaler().fit_transform(data)
+
+# 4.2
+#sd_scaled = StandardScaler().fit_transform(data)
+
+# 4.3
+#pca = PCA()
+#principal_components = pca.fit_transform(data)
+
+# 4.4
+
+
+# Plot the pairplot with the filtered data and "species" as hue
+# sns.pairplot(data, hue="species")
+# plt.show()
